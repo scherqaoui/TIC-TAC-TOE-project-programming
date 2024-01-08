47c4756b9c15de99cf4bdde6d97135e63ceb71ba
scherqaoui <152999139+scherqaoui@users.noreply.github.com>
2024-01-08 12:27:49 +0100

    game_setup.py

--- game_setup.py
+++ game_setup.py
@@ -10,17 +10,18 @@ Created on Thu Jan  4 17:12:37 2024
 
 import random
 
+ 
 def start_new_game(players, label, game_btns, next_turn):
     
     player = random.choice(players)
-
+      # Randomly choose a player to start the new game
     label.config(text=(player + "turn"))
-
+      # Update the label to indicate the current player's turn
     for row in range(3):
         for col in range(3):
             game_btns[row][col].config(text="", bg="grey")
-                     
-    return player
+            # Set the text on the button to an empty string and changes the background color to grey.         
+    return player # Return the player who will start the new game
 
 
 
